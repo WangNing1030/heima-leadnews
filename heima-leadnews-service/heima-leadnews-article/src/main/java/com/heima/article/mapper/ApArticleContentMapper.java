3b659862a3e3599fa39c145e1b20a4d5ee9ea3b4
WangNing <2533982657@qq.com>
2023-03-03 21:36:26 +0800

    test: 测试生成文章详情静态页

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,12 @@
+package com.heima.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.article.pojos.ApArticleContent;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author WangNing
+ */
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
