6c7757bb439295754868eca5f3aa8b21b8873155
WangNing <2533982657@qq.com>
2023-03-05 00:23:54 +0800

    style: 更改包名common.swagger

--- heima-leadnews-common/src/main/java/com/heima/common/swagger/Swagger2Configuration.java
+++ heima-leadnews-common/src/main/java/com/heima/common/swagger/Swagger2Configuration.java
@@ -1,4 +1,4 @@
-package com.heima.common.config;
+package com.heima.common.swagger;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
@@ -9,36 +9,37 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * Swagger配置类
- *
  * @author WangNing
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
-    @Bean
-    public Docket buildDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(buildApiInfo())
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
+public class Swagger2Configuration {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                //分组名称
+                .groupName("1.0")
                 .select()
-                // 要扫描的API(Controller)基础包
+                //这里指定Controller扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.heima"))
                 .paths(PathSelectors.any())
                 .build();
+        return docket;
     }
 
-    private ApiInfo buildApiInfo() {
-        Contact contact = new Contact("黑马程序员", "", "");
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("黑马头条-平台管理API文档")
-                .description("黑马头条后台api")
-                .contact(contact)
-                .version("1.0.0").build();
+                .title("黑马头条API文档")
+                .description("黑马头条API文档")
+                .version("1.0")
+                .build();
     }
 }
