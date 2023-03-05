e84010757f3f766036f7e4bb6d21ac0f22808f76
WangNing <2533982657@qq.com>
2023-03-05 21:11:28 +0800

    test: 自媒体文章自动审核功能测试

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/service/WmNewsAutoScanServiceTest.java
@@ -0,0 +1,28 @@
+package com.heima.wemedia.service;
+
+import com.heima.wemedia.WemediaApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author WangNing
+ */
+@SpringBootTest(classes = WemediaApplication.class)
+@RunWith(SpringRunner.class)
+public class WmNewsAutoScanServiceTest {
+
+    @Autowired
+    private WmNewsAutoScanService wmNewsAutoScanService;
+
+    /**
+     * 自媒体文章审核
+     */
+    @Test
+    public void autoScanWmNews() {
+
+        wmNewsAutoScanService.autoScanWmNews(6232);
+    }
+}
