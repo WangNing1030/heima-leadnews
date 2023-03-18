8523a53cbceb7fd493b94426183a2acccc70ad00
WangNing <2533982657@qq.com>
2023-03-18 19:01:55 +0800

    feat: 增加ES支持app端从ES中搜索文章

---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/ArticleSearchService.java
@@ -0,0 +1,21 @@
+package com.heima.search.service;
+
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.UserSearchDto;
+
+import java.io.IOException;
+
+/**
+ * @author WangNing
+ */
+public interface ArticleSearchService {
+
+
+    /**
+     * es 文章分页检索
+     *
+     * @param dto
+     * @return
+     */
+    ResponseResult search(UserSearchDto dto) throws IOException;
+}
